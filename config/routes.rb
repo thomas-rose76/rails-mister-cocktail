770097b71cde69ceb97469ed88b145aa3ccb312b
Thomas Rose <thomas.c.n.rose@gmail.com>
2020-02-14 14:52:45 +0100

    nouvelle seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:new, :create, :index, :show] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:new, :create, :show] do
     resources :doses, only: [:new, :create, :update, :show, :delete]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
 require 'open-uri'
 require 'json'
 
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
 ingredients = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
 results = JSON.parse(ingredients)
 results["drinks"].each do |result|
   Ingredient.create(name: result["strIngredient1"])
 end
+
+
+mojito = Cocktail.new(name: "Mojito")
+mojito.doses << Dose.new(description: "2", ingredient: Ingredient.first )
+mojito.save
